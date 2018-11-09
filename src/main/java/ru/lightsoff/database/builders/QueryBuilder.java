7cfffa3a4a86841b70bcc63018209205b5fb2eeb
Андрей <5minutes.am@gmail.com>
2018-11-09 23:17:51 +0300

    Initial commit

--- src/main/java/ru/lightsoff/database/builders/QueryBuilder.java
+++ src/main/java/ru/lightsoff/database/builders/QueryBuilder.java
@@ -1,12 +1,14 @@
 package ru.lightsoff.database.builders;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class QueryBuilder {
-    private String primaryKey;
+    private String primaryKey = "";
     private ArrayList<String> fields = new ArrayList<>();
-    private String ascField;
-    private String descField;
+    private String ascField = "";
+    private String descField = "";
+    private String from = "";
     private boolean asc = false;
     private boolean desc = false;
 
@@ -53,7 +55,29 @@ public class QueryBuilder {
         return this;
     }
 
+    public QueryBuilder from(String table){
+        from = table;
+        return this;
+    }
+
     public String getRaw(){
-        return null;
+        try{
+            validation();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null
+    }
+
+    private void validation() throws SQLException{
+        if(primaryKey.contains(";"))
+            throw new SQLException();
+        if(ascField.contains(";"))
+            throw new SQLException();
+        if(descField.contains(";"))
+            throw new SQLException();
+        for(String it: fields)
+            if(it.contains(";"))
+                throw new SQLException();
     }
 }
