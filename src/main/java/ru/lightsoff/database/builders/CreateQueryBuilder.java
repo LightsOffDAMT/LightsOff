7fbf8083bb70a102cde34dfb1ebff05ba5bf5512
5minutesAM <5minutes.am@gmail.com>
2018-11-11 13:39:23 +0300

    Jdbc connection added, some bugs fixed

--- src/main/java/ru/lightsoff/database/builders/CreateQueryBuilder.java
+++ src/main/java/ru/lightsoff/database/builders/CreateQueryBuilder.java
@@ -55,7 +55,11 @@ public class CreateQueryBuilder {
         String query = "CREATE TABLE ";
         query += name + " (\n";
         for(int i = 0; i < columns.size(); i++){
-            query += "\t" + columns.get(i) + "\t" + types.get(i) + "\t" + contraints.get(i) + ",\n";
+            query += "\t" + columns.get(i) + "\t" + types.get(i) + "\t" + contraints.get(i);
+            if(i == columns.size() - 1)
+                query += "\n";
+            else
+                query += ",\n";
         }
         query += ");";
         return query;
