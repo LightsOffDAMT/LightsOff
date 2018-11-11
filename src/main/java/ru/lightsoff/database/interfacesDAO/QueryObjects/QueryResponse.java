d7e76bd9e3c62d31a605666ba284ec49377b2f14
5minutesAM <5minutes.am@gmail.com>
2018-11-11 16:43:09 +0300

    QueryResponse polished

--- src/main/java/ru/lightsoff/database/interfacesDAO/QueryObjects/QueryResponse.java
+++ src/main/java/ru/lightsoff/database/interfacesDAO/QueryObjects/QueryResponse.java
@@ -1,7 +1,34 @@
 package ru.lightsoff.database.interfacesDAO.QueryObjects;
 
-public interface QueryResponse<T> {
-    String getStatus();
-    Long getTime();
-    T getData();
+public class QueryResponse<T> {
+    private String status = "";
+    private long time = 0;
+    private T data;
+
+    public String getStatus() {
+        return null;
+    }
+
+    public Long getTime() {
+        return null;
+    }
+
+    public T getData() {
+        return null;
+    }
+
+    public QueryResponse<T> withStatus(String status){
+        this.status = status;
+        return this;
+    }
+
+    public QueryResponse<T> withTime(long time){
+        this.time = time;
+        return this;
+    }
+
+    public QueryResponse<T> withData(T data){
+        this.data = data;
+        return this;
+    }
 }
