ed5f9ae97b670eab3da043538710c41a432d3cda
5minutesAM <5minutes.am@gmail.com>
2018-11-11 13:20:14 +0300

    Jdbc connection added

--- src/main/java/ru/lightsoff/database/DatabaseApplication.java
+++ src/main/java/ru/lightsoff/database/DatabaseApplication.java
@@ -2,15 +2,22 @@ package ru.lightsoff.database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import ru.lightsoff.database.builders.ColumnType;
 import ru.lightsoff.database.builders.DeleteQueryBuilder;
 import ru.lightsoff.database.builders.QueryBuilder;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class DatabaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DatabaseApplication.class, args);
+        ApplicationContext context = SpringApplication.run(DatabaseApplication.class, args);
     }
 
 }
