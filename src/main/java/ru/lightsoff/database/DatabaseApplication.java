9667d2d52ae38e7001149154ad5a1d4dfeaa9440
5minutesAM <5minutes.am@gmail.com>
2018-12-04 00:11:24 +0300

    --Added Eureka-client integration and properties for service-discovery

--- src/main/java/ru/lightsoff/database/DatabaseApplication.java
+++ src/main/java/ru/lightsoff/database/DatabaseApplication.java
@@ -1,24 +1,18 @@
 package ru.lightsoff.database;
 
-import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.config.EnableWebFlux;
-import ru.lightsoff.database.DAO.PlayerDAO;
-import ru.lightsoff.database.DAO.QueryObjects.QueryResponse;
-import ru.lightsoff.database.Entities.Player;
 import ru.lightsoff.database.builders.ColumnType;
 import ru.lightsoff.database.builders.QueryFactory;
-import ru.lightsoff.database.storage.StorageManager;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
-import java.util.ArrayList;
-
 @SpringBootApplication
 @EnableWebFlux
+@EnableEurekaClient
 public class DatabaseApplication {
 
     public static void main(String[] args) throws SQLException {
